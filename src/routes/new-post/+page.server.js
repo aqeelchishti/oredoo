3bad7b219acdf050526b59980c454ffdee33bc32
aqeelchishti <git@github.com>
2024-06-27 11:14:14 +0500

    update 14

--- src/routes/new-post/+page.server.js
+++ src/routes/new-post/+page.server.js
@@ -1,7 +1,6 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals: { safeGetSession } }) => {
-	//const next = url.searchParams.get('next') ?? '/new-post';
 	const { session, user } = await safeGetSession();
 
 	if (!session && !user) {
